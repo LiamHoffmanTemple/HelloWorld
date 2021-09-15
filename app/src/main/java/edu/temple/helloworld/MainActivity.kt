f9e0b1ca8bb46efa989b6b6361f456100b221018
Liam <Liam@DESKTOP-BBOI67G>
2021-09-14 22:13:41 -0400

    Added button, changes hello world text on click

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -2,10 +2,16 @@ package edu.temple.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var Text = findViewById<TextView>(R.id.textView)
+
+        findViewById<Button>(R.id.Button).setOnClickListener({Text.text = "You clicked the button"})
     }
 }
